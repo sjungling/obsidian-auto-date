18d939769b7d29393842bd1852425a92288217a1
Scott Jungling <scott@moderne.io>
2024-04-14 20:03:17 -0700

    refactor: use `vault.on("create")` to handle new notes without vault reload

--- main.ts
+++ main.ts
@@ -1,18 +1,19 @@
-import { Plugin } from "obsidian";
+import { Plugin, TAbstractFile, TFile } from "obsidian";
 
 export default class AutoDatePlugins extends Plugin {
 	async onload() {
-		this.app.workspace.onLayoutReady(() => {
-			console.info("AutoDate plugin loaded");
-			this.app.vault.getMarkdownFiles().forEach((file) => {
-				this.app.fileManager.processFrontMatter(file, (properties) => {
-					if (!properties?.date) {
-						const date = new Date(file.stat.ctime);
-						const formattedDate = date.toISOString().slice(0, 10);
-						properties.date = formattedDate;
-					}
-				});
+		this.app.vault.on("create", this.upsertDate.bind(this));
+	}
+
+	upsertDate(file: TAbstractFile) {
+		if (file instanceof TFile) {
+			this.app.fileManager.processFrontMatter(file, (properties) => {
+				if (!properties?.date) {
+					const date = new Date(file.stat.ctime);
+					const formattedDate = date.toISOString().slice(0, 10);
+					properties.date = formattedDate;
+				}
 			});
-		});
+		}
 	}
 }
